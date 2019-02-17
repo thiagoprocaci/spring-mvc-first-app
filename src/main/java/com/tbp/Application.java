621bb1bca5ec2bf970aca06b291928b70f43c7d2
thiago <thiagoprocaci@gmail.com>
2019-02-17 12:54:00 -0300

    utf-8

--- src/main/java/com/tbp/Application.java
+++ src/main/java/com/tbp/Application.java
@@ -2,8 +2,9 @@ package com.tbp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 
 @SpringBootApplication
@@ -14,6 +15,15 @@ public class Application  {
     }
 
 
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setForceEncoding(true);
+        characterEncodingFilter.setEncoding("UTF-8");
+        registrationBean.setFilter(characterEncodingFilter);
+        return registrationBean;
+    }
 
 
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,9 +1,9 @@
+<%@ page language="java" pageEncoding="UTF-8"%>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <html>
-    <head>
-        <meta charset="UTF-8">
-
-
-    </head>
+   <head>
+   	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   </head>
     <body>
 
       <a href="<%=request.getContextPath()%>/person/list">Person CRUD</a>
